8239e2a8b0dc9fc423753005a966d228eb0c46b3
Valeri Karpov <val@karpov.io>
2025-04-02 06:23:53 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  darkMode: 'class',
+  content: ['./public/*.html', './public/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        'red-berry': {
+    '50': '#ffeeee',
+    '100': '#ffdada',
+    '200': '#ffbbbb',
+    '300': '#ff8b8b',
+    '400': '#ff4949',
+    '500': '#ff1111',
+    '600': '#ff0000',
+    '700': '#e70000',
+    '800': '#be0000',
+    '900': '#880000',
+    '950': '#560000',
+},
+
+      }
+    },
+  },
+  plugins: [],
+};
