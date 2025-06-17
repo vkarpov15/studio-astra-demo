ddea802450b859f3e24c0475102d62e1a476eec1
Valeri Karpov <val@karpov.io>
2025-06-17 17:24:30 -0400

    initial commit

--- netlify/functions/studio.js
+++ netlify/functions/studio.js
@@ -1,19 +1,37 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const { driver, createAstraUri } = require('@datastax/astra-mongoose');
+
+const mongodbMongoose = new mongoose.Mongoose();
+
+mongoose.setDriver(driver);
+mongoose.set('autoCreate', false);
+mongoose.set('autoIndex', false);
 
 require('../../src/movies-demo/movies.model');
 
-const handler = require('@mongoosejs/studio/backend/netlify')({
-  apiKey: process.env.MONGOOSE_STUDIO_API_KEY
+const handler = require('@mongoosejs/studio/backend/netlify')(mongoose, {
+  apiKey: process.env.MONGOOSE_STUDIO_API_KEY,
+  studioConnection: mongodbMongoose
 }).handler;
 
 let conn = null;
+let mongodbConn = null;
 
 module.exports = {
   handler: async function studioHandler(params) {
+    if (mongodbConn == null) {
+      mongodbConn = await mongodbMongoose.connect(process.env.MONGODB_CONNECTION_STRING, { serverSelectionTimeoutMS: 3000 });
+    }
     if (conn == null) {
-      conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { serverSelectionTimeoutMS: 3000 });
+      conn = await mongoose.connect(
+        createAstraUri(
+          process.env.ASTRA_ENDPOINT,
+          process.env.ASTRA_TOKEN,
+          process.env.ASTRA_KEYSPACE
+        )
+      );
     }
 
     return handler.apply(null, arguments);
