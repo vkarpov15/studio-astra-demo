2a79c4b5b6f71401ba04feedb20ae7d1521a8225
Valeri Karpov <val@karpov.io>
2025-06-18 16:29:56 -0400

    experiment with mongoose overwrite middleware result

--- src/movies-demo/movies.model.js
+++ src/movies-demo/movies.model.js
@@ -46,4 +46,12 @@ const schema = new mongoose.Schema({
   num_mflix_comments: Number
 });
 
+schema.post('countDocuments', function (err, res, next) {
+  console.log('CountDocuments error', err);
+  if (err.message.includes('Too many documents to count')) {
+    return next(null, mongoose.overwriteMiddlewareResult('1000+'));
+  }
+  return next(err);
+});
+
 module.exports = mongoose.model('Movie', schema, 'movies');
